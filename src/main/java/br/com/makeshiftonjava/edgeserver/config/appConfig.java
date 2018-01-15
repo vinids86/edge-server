76378fbc554aa8a0232dd004c4f64b5b5daf1d53
Vinicius.saraiva Acc <viniciusdsaraiva@gmail.com>
2018-01-15 00:47:33 -0200

    Add banner

---
+++ src/main/java/br/com/makeshiftonjava/edgeserver/config/appConfig.java
@@ -0,0 +1,29 @@
+package br.com.makeshiftonjava.edgeserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class appConfig {
+
+
+//    TODO Zull routes fallback
+//    @Bean
+//    public FallbackProvider zuulFallbackProvider() {
+//        return new FallbackProvider() {
+//            @Override
+//            public ClientHttpResponse fallbackResponse(Throwable cause) {
+//                return null;
+//            }
+//
+//            @Override
+//            public String getRoute() {
+//                return "anotherRoute";
+//            }
+//
+//            @Override
+//            public ClientHttpResponse fallbackResponse() {
+//                return null;
+//            }
+//        };
+//    }
+}
